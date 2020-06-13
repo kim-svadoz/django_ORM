e2a30a23e7652da7fb5e36fdaf0edd842ab34732
kim-svadoz <dhkdghehfdl@gmail.com>
2020-06-13 17:30:28 +0900

    django project

---
+++ mysite/articles/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+def index(request):
+    return render(request, 'articles/index.html')
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    title = request.GET.get('title')
+    text = request.GET.get('text')
+    context = {
+        'title' : title,
+        'text' : text
+    }
+    return render(request, 'articles/create.html', context)
